99f008836291f4f876238e490fa24b182a0a7aa7
bwesterhof <bwesterhof@bol.com>
2020-01-29 12:52:50 +0100

    dag-exercise7

--- dags/dag_exercise7.py
+++ dags/dag_exercise7.py
@@ -2,7 +2,7 @@ import json
 import pathlib
 import posixpath
 import requests
-from dags.hooks.launch_hook import LaunchHook
+from hooks.launch_hook import LaunchHook
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 
